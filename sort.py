4f586798ecbc6f77d6813e6fed44931efeb22601
Serhat Yildirim <serhat.yildirim@hetic.net>
2019-06-04 01:34:29 +0200

    (add :sparkles:): selection()

--- sort.py
+++ sort.py
@@ -1,6 +1,6 @@
 
 
-TAB = [2,8,4,9,2]
+TAB = [23, 8, 4, 9, 18, 1, 39, 3, 98, 0, 12]
 
 class Sort: 
     def __init__(self, tab):
@@ -10,6 +10,11 @@ class Sort:
     # Buble sort
     # Complexity : O(n) 
     def buble(self):
+        """
+        Returns:
+           The sorted table
+        """
+
         check = False
         tab = self.tab
         tab_length = len(self.tab)-1
@@ -24,13 +29,37 @@ class Sort:
                     check = False
 
             tab_length-=1 
-        print('after', tab)     
+
+        return tab     
 
 
     #Selection
     #Complexity O(n^2)
     def selection(self):
-        pass
+        """
+           Selection sort function
+        """
+
+        tab = self.tab
+        tab_length = len(self.tab)
+        print(tab)
+     
+        for i in range(tab_length):
+            min_item = i
+            j = i +1
+
+            while j < tab_length:
+                if tab[j] < tab[min_item]:
+                    min_item = j
+                j+=1
+        
+            if i != min_item:
+                tab[i], tab[min_item] = tab[min_item], tab[i] 
+            
+        print(tab)
+            
+            
+        
 
 
 
@@ -38,4 +67,5 @@ class Sort:
 
 
 sort = Sort(TAB)
-sort.buble()
+# sort.buble()
+sort.selection()
